ac5b9e510923db485ef31448221748ebc4c81116
Prakriti-git <prakreetiuprety.58@gmail.com>
2023-05-25 13:53:41 +0545

    my first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var list = new List<StudentVm>() {
+                new StudentVm() {Id= 1, Name = "One", Address="Addr 1"},
+                 new StudentVm() {Id= 1, Name = "Two", Address="Addr 2"}
+            };
+            return View(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/StudentVm.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class StudentVm
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        
+    }
+}
